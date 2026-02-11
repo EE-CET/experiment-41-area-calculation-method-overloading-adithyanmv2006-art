a6a2698521820d5f293a0f1e236997dae9c1f0a7
Adithyan <adithyanmv2006@gmail.com>
2026-02-12 00:58:16 +0530

    Error corrected

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 public class AreaCalculator{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        int l=sc.nextInt();
-        int b=sc.nextInt();
-        int ba=sc.nextInt();
-        int h=sc.nextInt();
-        double carea=Math.PI*r*r;
-        double rarea=l*b;
-        double tarea=0.5*ba*h;
-        System.out.println(String.format("%.2f",carea));
-        System.out.println(String.format("%.2f",rarea));
-        System.out.println(String.format("%.2f",tarea));
-    }
+
+	static void calculatearea(float radius){
+		double carea=Math.PI*radius*radius;
+		System.out.println(String.format("%.2f",carea));
+	}
+	static void calculatearea(float length,float breadth){
+		double rarea=length*breadth;
+		System.out.println(String.format("%.2f",rarea));
+	}
+	static void calculatearea(double base,double height){
+		double tarea=0.5*base*height;
+		System.out.println(String.format("%.2f",tarea));
+	}
+    	public static void main(String[] args){
+        	Scanner sc=new Scanner(System.in);
+        	float r=sc.nextFloat();
+        	float l=sc.nextFloat();
+        	float b=sc.nextFloat();
+        	double ba=sc.nextDouble();
+        	double h=sc.nextDouble();
+
+		calculatearea(r);
+		calculatearea(l,b);
+		calculatearea(ba,h);
+		
+       	}
 }
